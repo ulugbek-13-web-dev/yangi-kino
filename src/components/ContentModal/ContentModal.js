90699f8ebe9d443a08f07f81e741bb954b6c2a0d
Ulugbek Davronov <75253316+ulugbek-13-web-dev@users.noreply.github.com>
2021-08-10 22:39:48 +0500

    Update ContentModal.js
    .env not important

--- src/components/ContentModal/ContentModal.js
+++ src/components/ContentModal/ContentModal.js
@@ -48,7 +48,7 @@ export default function TransitionsModal({ children, media_type, id }) {
 
   const fetchData = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=d54f1a6164bfe4d1e9a8e04d6e9d345c&language=en-US`
     );
 
     setContent(data);
